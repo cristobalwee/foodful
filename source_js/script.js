7b7e00b8ac3de3eed39edbc53af5d7296c8117cc
cristobalwee <cristobal.wee@gmail.com>
2016-11-27 23:51:01 +0000

    animation fixes

--- source_js/script.js
+++ source_js/script.js
@@ -2,9 +2,11 @@ var position = 0;
 var views = ['#/', '#/facts', '#/purpose', '#/legal', '#/contact'];
 
 $("body").bind("mousewheel", function(e){
-  var $mouse = $('#scrollanim');
-  TweenLite.to($mouse, 0.5, {bottom: "-100px", delay: 0.75});
-  animate(views[position]);
+  if(e.originalEvent.wheelDelta /120 < 0) {
+    var $mouse = $('#scrollanim');
+    TweenLite.to($mouse, 0.75, {bottom: "-100px", delay: 0.5});
+    animate(views[position]);
+  };
 });
 
 
@@ -21,18 +23,35 @@ $("body").on("mousewheel", function(e) {
       window.location.href = views[position];
     }
   }
-}, 1000);
+}, 750);
 
-var animate = function(view) {
-  switch(view) {
+$(".homelink").on("click", function() {
+  position = 0;
+});
+
+$(".link").on("click", function() {
+  alert("link clicked");
+  return true;
+});
+
+var animate = function(input) {
+  switch(input) {
     case '#/':
-      console.log("tweening...");
-      var $landing = $('#landing');
-      console.log($landing);
-      TweenLite.to($landing, 0.75, {position: "absolute", top: "500px", backgroundColor: "rgba(255, 255, 255, 0)",ease: Power2.easeInOut});
-      console.log("twoned");
+      var $view = $('#landing');
+      break;
+    case '#/facts':
+      var $view = $('#facts');
+      break;
+    case '#/purpose':
+      var $view = $('#purpose');
+      break;
+    case '#/legal':
+      var $view = $('#legal');
+      break;
+    case '#/contact':
       break;
     default:
       alert("ok");
   }
+  TweenLite.to($view, 1, {position: "absolute", top: "1000px", backgroundColor: "rgba(255, 255, 255, 0)",ease: Power3.easeInOut});
 };
