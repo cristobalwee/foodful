78258fe9d416774d9a26ea5db2a01cf8812bce8d
cristobalwee <cristobal.wee@gmail.com>
2016-11-27 00:00:44 +0000

    landing page commit

--- source_js/script.js
+++ source_js/script.js
@@ -1,8 +1,19 @@
+var position = 0;
+var views = ['#/', '#/facts', '#/purpose', '#/legal', '#/contact'];
+
 $("body").bind("mousewheel", function(e){
-    if(e.originalEvent.wheelDelta /120 > 5) {
-      alert("scrolled up");
+    if(e.originalEvent.wheelDelta /120 > 0) {
+      if (position > 0) {
+        position--;
+        window.location.href = views[position];
+        e.stopPropation();
+      }
     }
-    if(e.originalEvent.wheelDelta /120 < -5){
-      window.location.href = '#/facts';
+    if(e.originalEvent.wheelDelta /120 < 0){
+      if (position < 4) {
+        position++;
+        window.location.href = views[position];
+        e.stopPropation();
+      }
     }
 });
