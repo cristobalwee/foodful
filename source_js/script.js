7315448a833a0f98cfa9c5bc57424e74ccbe2a46
cristobalwee <cristobal.wee@gmail.com>
2016-12-01 20:23:56 +0000

    fixed indicators

--- source_js/script.js
+++ source_js/script.js
@@ -1,33 +1,44 @@
 var position = 0;
 var views = ['#/', '#/facts', '#/purpose', '#/legal', '#/contact'];
+var scrollflag = true;
 
 $("body").bind("mousewheel", function(e) {
   var $indicator = $(".currentposition");
-  if (position < 4) {
-    TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
-  }
-  if (e.originalEvent.wheelDelta /120 > 0 && position == 4) {
-    TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
-    var $footer = $('.footer');
-    TweenLite.to($footer, 0.75, {bottom: "-100px"});
-  }
-  if(e.originalEvent.wheelDelta /120 < 0) {
-    var $mouse = $('#scrollanim');
-    TweenLite.to($mouse, 0.75, {bottom: "-100px", delay: 0.25});
+  if (scrollflag) {
+    if (e.originalEvent.wheelDelta /120 < 0 && position < 4) {
+      TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
+    };
+    if (e.originalEvent.wheelDelta /120 > 0) {
+      TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
+      if (position == 4) {
+        var $footer = $('.footer');
+        TweenLite.to($footer, 0.75, {bottom: "-100px"});
+      };
+    };
+    if(e.originalEvent.wheelDelta /120 < 0) {
+      var $mouse = $('#scrollanim');
+      TweenLite.to($mouse, 0.75, {bottom: "-100px", delay: 0.25});
+    };
+
+    if(e.originalEvent.wheelDelta /120 < 0) {
+      enter(views[position]);
+    };
+    if (e.originalEvent.wheelDelta /120 > 0) {
+      exit(views[position]);
+    };
+    scrollflag = false;
   };
 });
 
 $("body").bind("mousewheel", function(e) {
-  if(e.originalEvent.wheelDelta /120 < 0) {
-    enter(views[position]);
+  if (!scrollflag) {
+    scrollflag = true;
   };
-  if (e.originalEvent.wheelDelta /120 > 0) {
-    exit(views[position]);
-  };
-}, 50);
+}, 150);
 
 var enter = function(partial) {
   var tween = true;
+  var $delay = 0;
   switch(partial) {
     case '#/':
       var $view = $('#landing');
@@ -51,10 +62,15 @@ var enter = function(partial) {
   if (!tween) {
     return;
   };
-  TweenLite.to($view, 1, {position: "absolute", top: "-750px", autoAlpha: 0,ease: Power3.easeInOut});
+
+  if (position == 1) {
+    $delay = 0.15;
+  };
+
+  TweenLite.to($view, 1, {position: "absolute", top: "-750px", autoAlpha: 0, ease: Power3.easeInOut, delay: $delay});
   setTimeout(function() {
     window.location.href = views[position];
-  }, 1050);
+  }, 950);
 };
 
 var exit = function(partial) {
@@ -82,60 +98,19 @@ var exit = function(partial) {
   if (!tween) {
     return;
   };
-  TweenLite.to($view, 1, {position: "absolute", top: "750px", autoAlpha: 0,ease: Power3.easeInOut});
+
+  TweenLite.to($view, 1, {position: "absolute", top: "750px", autoAlpha: 0, ease: Power3.easeInOut});
   setTimeout(function() {
     window.location.href = views[position];
-  }, 2050);
+  }, 950);
 };
-
-/*$("body").bind("mousewheel", function(e){
-  var $indicator = $(".currentposition");
-  if (position < 4) {
-    TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
-  }
-  if (e.originalEvent.wheelDelta /120 > 0 && position == 4) {
-    TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
-    var $footer = $('.footer');
-    TweenLite.to($footer, 0.75, {bottom: "-100px"});
-  }
-  if(e.originalEvent.wheelDelta /120 < 0) {
-    var $mouse = $('#scrollanim');
-    TweenLite.to($mouse, 0.75, {bottom: "-100px", delay: 0.5});
-  };
-  if (e.originalEvent.wheelDelta /120 > 0) {
-    if (position > 0) {
-      animate(views[position]);
-    };
-  }
-  else {
-    if (position < 4) {
-      animate(views[position]);
-    }
-  };
-});
-
-
-$("body").on("mousewheel", function(e) {
-  if(e.originalEvent.wheelDelta /120 > 0) {
-    if (position > 0) {
-      position--;
-      window.location.href = views[position];
-    }
-  }
-  if(e.originalEvent.wheelDelta /120 < 0){
-    if (position < 4) {
-      position++;
-      window.location.href = views[position];
-    }
-  }
-}, 750);*/
 
 $(".homelink").on("click", function() {
   console.log($(this).data("link"));
 });
 
-var resetPosition = function() {
-  position = 0;
+var setPosition = function(idx) {
+  position = idx;
 };
 
 var animate = function(input) {
