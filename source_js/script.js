b5331a63979948b319a75628662fcaad1d9453c4
cristobalwee <cristobal.wee@gmail.com>
2016-12-02 04:34:19 +0000

    added mobile media queries

--- source_js/script.js
+++ source_js/script.js
@@ -1,108 +1,116 @@
+var isMobile = false;
+if (screen.width <= 640) {
+  isMobile = true;
+  console.log(isMobile);
+};
+
 var position = 0;
 var views = ['#/', '#/facts', '#/purpose', '#/legal', '#/contact'];
 var scrollflag = true;
 
-$("body").bind("mousewheel", function(e) {
-  var $indicator = $(".currentposition");
-  if (scrollflag) {
-    if (e.originalEvent.wheelDelta /120 < 0 && position < 4) {
-      TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
-    };
-    if (e.originalEvent.wheelDelta /120 > 0) {
-      TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
-      if (position == 4) {
-        var $footer = $('.footer');
-        TweenLite.to($footer, 0.75, {bottom: "-100px"});
+if (!isMobile) {
+  $("body").bind("mousewheel", function(e) {
+    var $indicator = $(".currentposition");
+    if (scrollflag) {
+      if (e.originalEvent.wheelDelta /120 < 0 && position < 4) {
+        TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
       };
+      if (e.originalEvent.wheelDelta /120 > 0) {
+        TweenLite.to($indicator, 0.75, {scale: 0.5, backgroundColor: "#d0e8f9"});
+        if (position == 4) {
+          var $footer = $('.footer');
+          TweenLite.to($footer, 0.75, {bottom: "-100px"});
+        };
+      };
+      if(e.originalEvent.wheelDelta /120 < 0) {
+        var $mouse = $('#scrollanim');
+        TweenLite.to($mouse, 0.75, {bottom: "-100px", delay: 0.25});
+      };
+
+      if(e.originalEvent.wheelDelta /120 < 0) {
+        enter(views[position]);
+      };
+      if (e.originalEvent.wheelDelta /120 > 0) {
+        exit(views[position]);
+      };
+      scrollflag = false;
     };
-    if(e.originalEvent.wheelDelta /120 < 0) {
-      var $mouse = $('#scrollanim');
-      TweenLite.to($mouse, 0.75, {bottom: "-100px", delay: 0.25});
+  });
+
+  $("body").bind("mousewheel", function(e) {
+    if (!scrollflag) {
+      scrollflag = true;
+    };
+  }, 150);
+
+  var enter = function(partial) {
+    var tween = true;
+    var $delay = 0;
+    switch(partial) {
+      case '#/':
+        var $view = $('#landing');
+        position++;
+        break;
+      case '#/facts':
+        var $view = $('#facts');
+        position++;
+        break;
+      case '#/purpose':
+        var $view = $('#purpose');
+        position++;
+        break;
+      case '#/legal':
+        var $view = $('#legal');
+        position++;
+        break;
+      default:
+        tween = false;
+    };
+    if (!tween) {
+      return;
     };
 
-    if(e.originalEvent.wheelDelta /120 < 0) {
-      enter(views[position]);
+    if (position == 1) {
+      $delay = 0.15;
     };
-    if (e.originalEvent.wheelDelta /120 > 0) {
-      exit(views[position]);
+
+    TweenLite.to($view, 1, {position: "absolute", top: "-750px", autoAlpha: 0, ease: Power3.easeInOut, delay: $delay});
+    setTimeout(function() {
+      window.location.href = views[position];
+    }, 950);
+  };
+
+  var exit = function(partial) {
+    var tween = true;
+    switch(partial) {
+      case '#/facts':
+        var $view = $('#facts');
+        position--;
+        break;
+      case '#/purpose':
+        var $view = $('#purpose');
+        position--;
+        break;
+      case '#/legal':
+        var $view = $('#legal');
+        position--;
+        break;
+      case '#/contact':
+        var $view = $('#contact');
+        position--;
+        break;
+      default:
+        tween = false;
     };
-    scrollflag = false;
-  };
-});
+    if (!tween) {
+      return;
+    };
 
-$("body").bind("mousewheel", function(e) {
-  if (!scrollflag) {
-    scrollflag = true;
+    TweenLite.to($view, 1, {position: "absolute", top: "750px", autoAlpha: 0, ease: Power3.easeInOut});
+    setTimeout(function() {
+      window.location.href = views[position];
+    }, 950);
   };
-}, 150);
-
-var enter = function(partial) {
-  var tween = true;
-  var $delay = 0;
-  switch(partial) {
-    case '#/':
-      var $view = $('#landing');
-      position++;
-      break;
-    case '#/facts':
-      var $view = $('#facts');
-      position++;
-      break;
-    case '#/purpose':
-      var $view = $('#purpose');
-      position++;
-      break;
-    case '#/legal':
-      var $view = $('#legal');
-      position++;
-      break;
-    default:
-      tween = false;
-  };
-  if (!tween) {
-    return;
-  };
-
-  if (position == 1) {
-    $delay = 0.15;
-  };
-
-  TweenLite.to($view, 1, {position: "absolute", top: "-750px", autoAlpha: 0, ease: Power3.easeInOut, delay: $delay});
-  setTimeout(function() {
-    window.location.href = views[position];
-  }, 950);
-};
-
-var exit = function(partial) {
-  var tween = true;
-  switch(partial) {
-    case '#/facts':
-      var $view = $('#facts');
-      position--;
-      break;
-    case '#/purpose':
-      var $view = $('#purpose');
-      position--;
-      break;
-    case '#/legal':
-      var $view = $('#legal');
-      position--;
-      break;
-    case '#/contact':
-      var $view = $('#contact');
-      position--;
-      break;
-    default:
-      tween = false;
-  };
-  if (!tween) {
-    return;
-  };
-
-  TweenLite.to($view, 1, {position: "absolute", top: "750px", autoAlpha: 0, ease: Power3.easeInOut});
-  setTimeout(function() {
-    window.location.href = views[position];
-  }, 950);
 };
 
 $(".homelink").on("click", function() {
@@ -113,32 +121,14 @@ var setPosition = function(idx) {
   position = idx;
 };
 
-var animate = function(input) {
-  switch(input) {
-    case '#/':
-      var $view = $('#landing');
-      var tween = true;
-      break;
-    case '#/facts':
-      var $view = $('#facts');
-      var tween = true;
-      break;
-    case '#/purpose':
-      var $view = $('#purpose');
-      var tween = true;
-      break;
-    case '#/legal':
-      var $view = $('#legal');
-      var tween = true;
-      break;
-    case '#/contact':
-      var $view = $('#contact');
-      var tween = true;
-      break;
-    default:
-      alert("ok");
-  }
-  if (tween) {
-    TweenLite.to($view, 1, {position: "absolute", top: "400px", autoAlpha:0 ,ease: Power3.easeInOut});
-  }
+var toggleMenu = function() {
+  document.getElementById("menu").style.display = "block";
+  document.getElementById("menu").style.opacity = 1;
+  document.getElementById("menu").style.zIndex = 4;
+};
+
+var closeMenu = function() {
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("menu").style.opacity = 0;
+  document.getElementById("menu").style.zIndex = -1;
 };
