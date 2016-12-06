b9a1d4fc58d938616703337471530ac3adb65b9a
Jerry <ygongdev@gmail.com>
2016-12-06 04:40:48 +0000

    profile shit

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -44,17 +44,23 @@ foodfulControllers.controller('AboutController', ['$scope', '$http', function($s
 foodfulControllers.controller('LoginController', ['$scope', '$location', 'UserAuth', function($scope, $location, UserAuth) {
     position = -1;
     document.body.style.overflow = "scroll";
-    $scope.loginData = {};
-    $scope.login = function() {
-        UserAuth.loginUser($scope.loginData).then(function(arg) {
-            UserAuth.saveToken(arg.data.token);
-            console.log('logged in!');
-            console.log(arg);
-            $location.path('profile');
-        }).catch(function(arg) {
-            console.log(arg);
-        });
-    };
+    // If there is no userlogged in, the login page 
+    if (UserAuth.currentUser() == null){
+        $scope.loginData = {};
+        $scope.login = function() {
+            UserAuth.loginUser($scope.loginData).then(function(arg) {
+                UserAuth.saveToken(arg.data.token);
+                console.log('logged in!');
+                console.log(arg);
+                $location.path('profile');
+            }).catch(function(arg) {
+                console.log(arg);
+            });
+        };
+    }else{
+        // redirect to login page
+        $location.path('/profile');
+    }
 }]);
 
 foodfulControllers.controller('RegisterController', ['$scope', '$location', 'UserAuth', 'GeoCoder', function($scope, $location, UserAuth, GeoCoder) {
@@ -128,6 +134,8 @@ foodfulControllers.controller('SearchController', ['$scope', '$http', 'NgMap', '
 }]);
 
 foodfulControllers.controller('ProfileController', ['$scope', '$http', 'Prof', function($scope, $http, Prof) {
+    position = -1;
+    document.body.style.overflow = "scroll";
   Prof.getProfile().success(function(data) {
     console.log(data);
     $scope.user = data.data;
@@ -140,6 +148,8 @@ foodfulControllers.controller('ProfileController', ['$scope', '$http', 'Prof', f
 }]);
 
 foodfulControllers.controller('EditProfileController', ['$scope', '$http', 'Prof', function($scope, $http, Prof) {
+     position = -1;
+    document.body.style.overflow = "scroll";
   Prof.getProfile().success(function(data) {
     $scope.displayText = "";
     $scope.showDisplay = false;
