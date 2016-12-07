71e79f20921d1041efbe1594d5601e7409f3770b
Jerry <ygongdev@gmail.com>
2016-12-07 06:18:18 +0000

    fixed AM PM

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -383,6 +383,7 @@ foodfulControllers.controller('EditProfileController', ['$scope', '$http', 'Prof
     $scope.states = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
 
     if($scope.user.start_hour > 12){
+      start.new_user.start_hour = +$scope.new_user.start_hour - 12;
       $scope.startTime = 'PM';
     }else {
       $scope.startTime = 'AM';
