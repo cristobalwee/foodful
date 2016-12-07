8b9f817828eb8c8caebb320fd370c9f34693fe78
Naveenk96 <nkumar10@illinois.edu>
2016-12-07 06:42:39 +0000

    status

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -377,8 +377,15 @@ foodfulControllers.controller('EditProfileController', ['$scope', '$http', 'Prof
 
   Prof.getProfile().success(function(data) {
     $scope.user = data.data;
-    $scope.new_user = $scope.user;
+    //$scope.new_user = $scope.user;
     $scope.startstate = $scope.user.state;
+    if($scope.user.typeID === 0) {
+      $scope.donoryes = "Donating"
+      $scope.donorno = "All Out";
+    } else {
+      $scope.donoryes = "Accepting"
+      $scope.donorno = "Filled";
+    }
 
     $scope.states = ["AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
 
@@ -400,18 +407,19 @@ foodfulControllers.controller('EditProfileController', ['$scope', '$http', 'Prof
         $scope.locstring += $scope.user.zipcode;
 
         if ($scope.startTime == 'PM') {
-            $scope.new_user.start_hour = +$scope.new_user.start_hour + +12;
+            $scope.user.start_hour = +$scope.user.start_hour + +12;
         }
         if ($scope.endTime == 'PM') {
-            $scope.new_user.end_hour = +$scope.new_user.end_hour + +12;
+            $scope.user.end_hour = +$scope.user.end_hour + +12;
         }
         GeoCoder.geocode({address: $scope.locstring}).then(function(result) {
-            $scope.new_user.loc = [];
-            $scope.new_user.loc[0] = result[0].geometry.location.lng();
-            $scope.new_user.loc[1] = result[0].geometry.location.lat();
-            console.log($scope.new_user);
-            Prof.updateProfile($scope.new_user).then(function(arg) {
+            $scope.user.loc = [];
+            $scope.user.loc[0] = result[0].geometry.location.lng();
+            $scope.user.loc[1] = result[0].geometry.location.lat();
+            console.log($scope.user);
+            Prof.updateProfile($scope.user).then(function(arg) {
                 console.log(arg);
+                $scope.user = arg.data;
                 $location.path('/profile');
                 alert('Successfully Updated User');
             }).catch(function(arg) {
@@ -420,7 +428,6 @@ foodfulControllers.controller('EditProfileController', ['$scope', '$http', 'Prof
         }).catch(function(arg) {
             console.log(arg);
         });
-        $location.path('/profile');
     };
   });
 }]);
